98cdd66f7ff83d829d9b4516a1e5469bdfcaffbb
stanleeeys <69662538+stanleeeys@users.noreply.github.com>
2021-02-28 11:42:22 -0600

    btnacceder

--- LaboratorioProgramacionUno/LaboratorioProgramacionUno/Program.cs
+++ LaboratorioProgramacionUno/LaboratorioProgramacionUno/Program.cs
@@ -17,7 +17,7 @@ namespace LaboratorioProgramacionUno
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmLogin());
+            Application.Run(new FrmMenu());
         }
     }
 }
